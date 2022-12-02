f9cb06f110b2c01c4eaaf55da491a82ecf6c1456
hoanrosi <hoanroi@gmail.com>
2022-12-02 10:22:34 +0700

    update form validate

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var username = document.querySelector("#username");
-var email = document.querySelector("#email");
-var password = document.querySelector("#password");
-var confirmPassword = document.querySelector("#Confirm-password");
-var form = document.querySelector("form");
+const form = document.querySelector("form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
 
 // show input error message
 function showError(input, message) {
@@ -12,6 +12,7 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// show success
 function showSuccess(input) {
   let parent = input.parentElement;
   let small = parent.querySelector("small");
@@ -27,10 +28,20 @@ function checkEmptyError(listInput) {
       isEmptyError = true;
       showError(input, "Vui long nhap lai");
     } else {
-      showError(input);
+      showSuccess(input);
     }
   });
-  return isEmptyError;
+}
+
+// Check email
+function checkEmail(input) {
+  const regexEmail =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regexEmail.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 // Check length
@@ -48,19 +59,6 @@ function checkLength(input, min, max) {
   showSuccess(input);
   return false; // khong loi gi return false
 }
-// Check email
-function checkEmail(input) {
-  const regexEmail =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  input.value = input.value.trim();
-  let isEmailError = !regexEmail.test(input.value);
-  if (regexEmail.test(input.value)) {
-    showSuccess(input);
-  } else {
-    showError(input, "Email is not valid");
-  }
-  return isEmailError;
-}
 
 // check password
 function checkMatchPassword(passwordInput, cfPasswordInput) {
@@ -72,17 +70,11 @@ function checkMatchPassword(passwordInput, cfPasswordInput) {
 }
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  let isUsenameLengthError = checkLength(username, 3, 10);
-  let isPasswordLengthError = checkLength(password, 3, 10);
-  let isEmptyError = checkEmptyError([
-    username,
-    email,
-    password,
-    confirmPassword,
-  ]);
-
-  let isEmailError = checkEmail(email);
-  let isMatchError = checkMatchPassword(password, confirmPassword);
+  e.preventDefault(); // Ngăn load trang
+  if (!checkEmptyError([username, email, password, confirmPassword])) {
+    checkLength(username, 3, 10);
+    checkLength(password, 6, 20);
+    checkEmail(email);
+    checkMatchPassword(password, confirmPassword);
+  }
 });
